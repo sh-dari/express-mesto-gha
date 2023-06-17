00d31b673f29e82686318a91a1f036d0382520c2
Дарья Шипилова <sh.dari@list.ru>
2023-06-17 13:41:22 +0300

    fix: joi

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (v) => /https*:\/\/[\w\S]{1,}/gi.test(v),
+      validator: (v) => /https*:\/\/[\w\S]{1,}/.test(v),
       message: 'Avatar validation failed',
     },
   },
--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,10 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required(),
     password: Joi.string().required(),
-  }).unknown(true),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https*:\/\/[\w\S]{1,}/),
+  }),
 }), createUser);
 router.post('/signin', celebrate({
   body: Joi.object().keys({
