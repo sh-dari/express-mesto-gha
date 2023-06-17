19e8a31305bc674ea3d9ba4ca8a57fc042b57297
Дарья Шипилова <sh.dari@list.ru>
2023-06-17 14:54:20 +0300

    fix: regex

--- routes/cards.js
+++ routes/cards.js
@@ -17,7 +17,7 @@ router.delete('/:cardId', celebrate({
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().regex(/https*:\/\/[\w\S]{1,}/),
   }),
 }), createCard);
 router.put('/:cardId/likes', celebrate({
--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https*:\/\/[\w\S]{1,}/),
+    avatar: Joi.string().regex(/https*:\/\/[\w\S]{1,}/),
   }),
 }), createUser);
 router.post('/signin', celebrate({
