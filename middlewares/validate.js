1762fc2255eb83a811a8242ee0edd04a3f2fd1a7
Дарья Шипилова <sh.dari@list.ru>
2023-06-17 19:04:58 +0300

    fix: validate

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,81 +1,56 @@
 const { celebrate, Joi } = require('celebrate');
 const { REG_EXP } = require('../utils/constants');
 
-module.exports.deleteCardValidate = (req, res, next) => {
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().length(24).hex().required(),
-    }),
-  });
-  next();
-};
+module.exports.deleteCardValidate = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+});
 
-module.exports.createCardValidate = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().regex(REG_EXP),
-    }),
-  });
-  next();
-};
+module.exports.createCardValidate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(REG_EXP),
+  }),
+});
 
-module.exports.likeCardValidate = (req, res, next) => {
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().length(24).hex().required(),
-    }),
-  });
-  next();
-};
+module.exports.likeCardValidate = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+});
 
-module.exports.createUserValidate = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(REG_EXP),
-    }),
-  });
-  next();
-};
+module.exports.createUserValidate = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(REG_EXP),
+  }),
+});
 
-module.exports.loginUserValidate = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  });
-  next();
-};
+module.exports.loginUserValidate = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+module.exports.getUserValidate = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+});
 
-module.exports.getUserValidate = (req, res, next) => {
-  celebrate({
-    params: Joi.object().keys({
-      userId: Joi.string().length(24).hex().required(),
-    }),
-  });
-  next();
-};
+module.exports.updateUserValidate = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+});
 
-module.exports.updateUserValidate = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  });
-  next();
-};
-
-module.exports.updateAvatarValidate = (req, res, next) => {
-  celebrate({
-    body: Joi.object().keys({
-      avatar: Joi.string().regex(REG_EXP),
-    }),
-  });
-  next();
-};
+module.exports.updateAvatarValidate = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(REG_EXP),
+  }),
+});
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,15 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
+const cardRoute = require('./cards');
+const userRoute = require('./users');
+const loginRoute = require('./login');
+
 const undefinedRoute = (req, res, next) => {
   next(new NotFoundError('Указан неправильный путь'));
 };
-const cardRoute = require('./cards');
-const userRoute = require('./users');
-const { createUser, login } = require('../controllers/users');
-const {
-  createUserValidate,
-  loginUserValidate,
-} = require('../middlewares/validate');
 
-router.post('/signup', createUserValidate, createUser);
-router.post('/signin', loginUserValidate, login);
+router.use('/', loginRoute);
 
 router.use(auth);
 router.use('/users', userRoute);
